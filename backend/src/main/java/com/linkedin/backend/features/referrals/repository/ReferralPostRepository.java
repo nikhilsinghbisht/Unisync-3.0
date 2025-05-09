5ace4a5d33d4bb3a75e8e708a2da1f5f765f8cbc
MohitPimoli <mohitpimoli31@gmail.com>
2025-05-09 12:27:35 +0530

    Add API to fetch created referral post

--- backend/src/main/java/com/linkedin/backend/features/referrals/repository/ReferralPostRepository.java
+++ backend/src/main/java/com/linkedin/backend/features/referrals/repository/ReferralPostRepository.java
@@ -16,6 +16,7 @@ public interface ReferralPostRepository extends JpaRepository<ReferralPost, Long
     @Query("SELECT rp FROM ReferralPost rp JOIN FETCH rp.referrer WHERE rp.referrer.id = :referrerId")
     List<ReferralPost> findByReferrerIdWithReferrer(@Param("referrerId") Long referrerId);
 
-    @Query("SELECT r FROM ReferralPost r WHERE r.id = :id")
+    @Query("SELECT r FROM ReferralPost r WHERE r.referrer.id = :id")
     List<ReferralPost> getPostsByReferrer(@Param("id") Long id);
+
 }
